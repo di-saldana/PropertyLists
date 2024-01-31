15988090a8f137e87d2c0db359128e6be581084b
di-saldana <dsl42@alu.ua.es>
2024-01-31 14:55:30 +0100

    Initial Commit

---
+++ PropertyLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PropertyLists
+//
+//  Created by Dianelys Saldaña on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
