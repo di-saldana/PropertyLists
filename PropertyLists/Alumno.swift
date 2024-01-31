d532ea9cced0a27f97bea37ddf60fb67251dce18
di-saldana <dsl42@alu.ua.es>
2024-01-31 19:37:45 +0100

    first commit

---
+++ PropertyLists/Alumno.swift
@@ -0,0 +1,13 @@
+//
+//  Alumno.swift
+//  PropertyLists
+//
+//  Created by Dianelys Saldaña on 1/31/24.
+//
+
+import Foundation
+
+struct Alumno : Codable  {
+    var nombre : String
+    var notas: Array<Float>
+}
--- PropertyLists/ViewController.swift
+++ PropertyLists/ViewController.swift
@@ -12,6 +12,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let a1 = Alumno(nombre: "Pepe", notas: [5, 8.5, 10])
+        let a2 = Alumno(nombre: "Eva", notas: [10, 9])
+        let alumnos = [a1,a2]
+        
+        do {
+            var urlDocs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let urlPlist = urlDocs.appendingPathComponent("alumnos.plist")
+            let encoder = PropertyListEncoder()
+            encoder.outputFormat = .xml
+            
+            let data = try encoder.encode(alumnos)
+            try data.write(to: urlPlist)
+            
+            print("Data saved successfully to \(urlPlist)")
+        } catch {
+            print("Error saving data: \(error.localizedDescription)")
+        }
     }
 
 
